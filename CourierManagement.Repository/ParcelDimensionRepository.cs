aedfb804bdeb32e74113dc8a543bb71db9317cf2
bala.sundaram <sachinmani@gmail.com>
2021-03-18 00:53:17 +1300

    Fixed typo which resulted in build issue

--- CourierManagement.Repository/ParcelDimensionRepository.cs
+++ CourierManagement.Repository/ParcelDimensionRepository.cs
@@ -13,7 +13,7 @@ namespace CourierManagement.Repository
             { ParcelSize.Small, new ParcelSizeDimensionPriceInfo(9.99, 9.99, 9.99, ParcelSize.Small, 1, 2)},
             { ParcelSize.Medium, new ParcelSizeDimensionPriceInfo(49.99, 49.99, 49.99, ParcelSize.Medium, 3, 2)},
             { ParcelSize.Large, new ParcelSizeDimensionPriceInfo(99.99, 99.99, 99.99, ParcelSize.Large, 6, 2)},
-            { ParcelSize.Large, new ParcelSizeDimensionPriceInfo(5000, 5000, 5000, ParcelSize.Xl, 10, 2)}
+            { ParcelSize.Xl, new ParcelSizeDimensionPriceInfo(5000, 5000, 5000, ParcelSize.Xl, 10, 2)}
         };
 
         public List<ParcelSizeDimensionPriceInfo> GetDimensions()
